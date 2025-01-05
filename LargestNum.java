fc20462c3a3caaf6f189f237142234c2f205c7b9
mohamedfahimyousuf <mohamedfahimyousuf786@gmail.com>
2025-01-05 14:26:56 +0530

    Largest numbers program

---
+++ LargestNum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class LargestNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int max =0;
+        System.out.println("Enter a number: ");
+        while(true){
+            int num = scanner.nextInt();
+            if(num == 0){
+                System.out.println("User enters 0");
+                break;
+            }
+            else if(num>max){
+                max = num;
+            }
+        }
+        System.out.println("The Largest Number: "+max);
+        scanner.close();
+    }
+}
