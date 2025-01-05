ba7de70c5814f0cd56b534b5029c63d24567e2eb
mohamedfahimyousuf <mohamedfahimyousuf786@gmail.com>
2025-01-05 13:06:07 +0530

    Subracting product and sum of digits program

---
+++ ProductAndSumOfDigits.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class ProductAndSumOfDigits{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int sum = 0;
+        int product = 1;
+        while(num>0){
+            int rem = num%10;
+            sum+=rem;
+            product*=rem;
+            num /= 10;
+        }
+        System.out.println("Sum of Digits: "+sum);
+        System.out.println("Product of Digits: "+product);
+        int sub = product - sum;
+        System.out.println("Result (product - sum) : "+sub);
+    }
+}
