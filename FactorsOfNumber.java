39feb375a2c6782b6c9002afc21eabe792bf2bbe
mohamedfahimyousuf <mohamedfahimyousuf786@gmail.com>
2025-01-05 13:19:55 +0530

    Factors of number program

---
+++ FactorsOfNumber.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class FactorsOfNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int i = 1;
+        System.out.println("Factors of number "+num+ " are:");
+        while(i<=num){
+            if(num%i==0){
+                System.out.print(i+" ");
+            }
+            i++;
+        }
+        scanner.close();
+    }
+}
