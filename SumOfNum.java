42a96472a36442d3db8016ec4719d1e6f44ee335
mohamedfahimyousuf <mohamedfahimyousuf786@gmail.com>
2025-01-05 13:32:17 +0530

    Sum of all numbers program

---
+++ SumOfNum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class SumOfNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        System.out.println("Enter a number: ");
+        while(true){
+            int num = scanner.nextInt();
+            if(num == 0){
+                System.out.println("The user enters 0");
+                break;
+            }
+            else{
+                sum+=num;
+            }
+        }
+        System.out.println("The sum of all numbers: "+sum);
+        scanner.close();
+    }
+}
