7c2c30f7a6636ac5c4281bd27d7115c51040aa88
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-02-10 23:15:17 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a number : ");
         int num = scanner.nextInt();
         int factorial = 1;
         for(int i=1;i<=num;i++){
