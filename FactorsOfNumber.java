c75e2a6e45092e2d5584dd393281cce3b4c708c9
Mohamed Fahim M <129580983+mohamedfahimyousuf@users.noreply.github.com>
2025-02-22 19:30:02 +0530

    Update FactorsOfNumber.java

--- FactorsOfNumber.java
+++ FactorsOfNumber.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class FactorsOfNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a number : ");
         int num = scanner.nextInt();
         int i = 1;
         System.out.println("Factors of number "+num+ " are:");
